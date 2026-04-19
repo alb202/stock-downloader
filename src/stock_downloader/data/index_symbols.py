2a0ff7d91bb0c1a58666417b72e31f53d3bc3859
alb202 <ablumenf@gmail.com>
2026-04-18 21:39:20 -0400

    Fixed position of dowjones symbol data

--- src/stock_downloader/data/index_symbols.py
+++ src/stock_downloader/data/index_symbols.py
@@ -7,7 +7,7 @@ class GetIndexSymbols:
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
+        # "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive",
         "Referer": "https://www.google.com/",  # Optional: Mimic a previous page
     }
@@ -20,7 +20,7 @@ class GetIndexSymbols:
 
     INDEX_LIST = {
         "sp500": {"url": "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies", "table_index": 0},
-        "dowjones": {"url": "https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average", "table_index": 2},
+        "dowjones": {"url": "https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average", "table_index": 1},
         "nasdaq100": {"url": "https://stockanalysis.com/list/nasdaq-100-stocks/", "table_index": 0},
     }
 
